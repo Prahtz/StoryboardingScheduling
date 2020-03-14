fc87224ed7fffaea503ef1042bace16b3cfffd2c
Lorenzo Pratesi <lorenzopratesi3@gmail.com>
2020-03-14 18:47:29 +0100

    Simulazioni su ALG2k

--- StoryboardingScheduling/src/scheduler/TestAlg2k.java
+++ StoryboardingScheduling/src/scheduler/TestAlg2k.java
@@ -1,6 +1,11 @@
 package scheduler;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.ListIterator;
+import java.util.Random;
 
 public class TestAlg2k {
 	public TestAlg2k() {}
@@ -47,6 +52,71 @@ public class TestAlg2k {
 		double a = alg.start();
 		double qc = chop.start();
 		System.out.println(k + " " + a + " " + qc);
+	}
+	
+	public boolean testAndWriteResults(LinkedList<LinkedList<Job>> inputList, BetaGenerator bg, String fileName) throws IOException {
+		File f = new File("csv/" + fileName);
+		f.delete();
+		f.createNewFile();
+		FileWriter fw = new FileWriter(f);
+		int n = 1000;
+		for(int i = 0; i < n; i++) {
+			ListIterator<LinkedList<Job>> it = inputList.listIterator();
+			double beta = bg.generateBeta();
+			int k = Service.generateKAlg2k(beta);
+			double c = 1/(Math.pow(beta, k-1)) *  max(1/Math.pow(beta,k-1), 
+					1/(1 - Math.pow(beta, 2*k)), 1 + (Math.pow(beta, 3*k) / (1 - Math.pow(beta, k))));
+			fw.write(beta + ";" + k + ";" + c + "\n;");
+			while(it.hasNext()) {
+				LinkedList<Job> input = it.next();
+				Alg2k alg = new Alg2k(k, beta, Service.cloneList(input));
+		 		QuantizedChop chop = new QuantizedChop(k, beta, Service.cloneList(input));
+				double algValue = alg.start();
+				double chopValue = chop.start();
+				fw.write(algValue + ";" + chopValue + ";");
+				if(c * algValue < (1/Math.pow(beta, k-1)) * chopValue) {
+					fw.flush();
+					fw.close();
+					return false;
+				}
+			}
+			fw.write("\n");
+		}
+		fw.flush();
+		fw.close();
+		return true;
+	}
+	
+	public void startTesting() throws IOException {
+		int n = 1000;
+		int jobNumber = 1000;
+		LinkedList<LinkedList<Job>> inputList = new LinkedList<LinkedList<Job>>();
+		for(int i = 0; i < n; i++)
+			inputList.add(Service.generateRandomInput(jobNumber, 4));
+		testAndWriteResults(inputList, getBetaGenerator(0, 0.3), "ALG2k0and0_3.csv");
+		testAndWriteResults(inputList, getBetaGenerator(0.3, 0.6), "ALG2k0_3and0_6.csv");
+		testAndWriteResults(inputList, getBetaGenerator(0.6, 0.8), "ALG2k0_6and0_8.csv");
+		testAndWriteResults(inputList, getBetaGenerator(0.8, 1), "ALG2k0_8and1.csv");
+	}
+	
+	private BetaGenerator getBetaGenerator(double a, double b) {
+		BetaGenerator bg = () -> {
+			Random random = new Random();
+			double beta;
+			do
+				beta = random.nextDouble();
+			while (beta == 0 || beta == 1 || !(beta > a && beta <= b));
+			return beta;
+		};
+		return bg;
+	}
+
+	private double max(double a, double b, double c) {
+		if(a >= b && a >= c)
+			return a;
+		else if(b >= a && b >= c)
+			return b;
+		return c;
 	}
 
 	
